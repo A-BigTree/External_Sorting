f1e127eb9e3e13e22d364d4c707bd53ce6a19612
Shuxin-Wang <81904920+A-BigTree@users.noreply.github.com>
2022-10-16 00:29:19 +0800

    Update phase4.cpp

--- Project/phase4.cpp
+++ Project/phase4.cpp
@@ -8,4 +8,42 @@ using namespace std;
 template<typename T>
 class MinLoserTree {
 
+private:
+	//败者树
+	int* ls;
+	//参赛数据
+	T* buffer;
+	//参赛数据长度
+	int LENGTH;
+	//设置参赛最小值
+	T MIN = -1;
+
+public:
+	//默认构造函数
+	MinLoserTree() {
+		ls = NULL;
+		buffer = NULL;
+		LENGTH = 0;
+	}
+
+	//构造函数
+	MinLoserTree(int length) {
+		ls = new int[length + 1];
+		buffer = new T[length + 1];
+		LENGTH = length;
+	}
+	
+	~MinLoserTree() {
+		delete[]ls;
+		delete[]buffer;
+	}
+
+	//设置最小值
+	void setMin(T& _MIN) {
+		MIN = _MIN;
+	}
+
+
+
+
 };
